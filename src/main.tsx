6fdbbe1350f2a35de7598b9e22fa0030421bf761
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-11-15 16:04:58 -0300

    feat: always take to login when you open de page

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,8 @@ createRoot(document.getElementById('root')!).render(
             clientId={import.meta.env.VITE_AUTH0_CLIENT_ID ?? ""}
             authorizationParams={{
                 redirect_uri: window.location.origin,
-                audience: import.meta.env.VITE_AUTH0_AUDIENCE ?? ""
+                audience: import.meta.env.VITE_AUTH0_AUDIENCE ?? "",
+                prompt: "login"
             }}
         >
             <PaginationProvider>
