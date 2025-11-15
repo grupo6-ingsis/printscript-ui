f657b09c90ae91315de7519a7f7d57cdb4dbf0a2
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-11-15 15:20:22 -0300

    feat: put only domain in secrets

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,6 @@ import {Auth0Provider} from "@auth0/auth0-react";
 
 console.log(import.meta.env.VITE_AUTH0_DOMAIN)
 console.log(import.meta.env.VITE_AUTH0_CLIENT_ID)
-console.log(import.meta.env.VITE_AUTH0_AUDIENCE)
 
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
