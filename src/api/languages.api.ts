2eb2523ef3f85d5f8a153cc86f5d268a7a956ade
Agustín Guglielmelli <agusguglielmelli@gmail.com>
2025-11-15 22:59:41 -0300

    .

--- src/api/languages.api.ts
+++ src/api/languages.api.ts
@@ -2,5 +2,6 @@ import apiClient from "./apiClient.ts";
 import {FileType} from "../types/FileType.ts";
 
 export async function getSupportedLanguages(): Promise<FileType[]> {
-    return apiClient.get('/language/supported');
+    const {data} = await apiClient.get('/language/supported');
+    return data;
 }
