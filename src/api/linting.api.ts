cf97cd5e19cbafd91fef948fe33ebe4e53c23122
Agustín Guglielmelli <agusguglielmelli@gmail.com>
2025-11-17 03:05:32 -0300

    fixed rules endpoints

--- src/api/linting.api.ts
+++ src/api/linting.api.ts
@@ -2,11 +2,11 @@ import apiClient from "./apiClient.ts";
 import {LintConfigDto, LintRuleDto} from "../types/Rule.ts";
 
 export async function getLintingRules(): Promise<LintRuleDto[]> {
-    const { data } = await apiClient.get(`/snippets/lintrule/all`);
+    const { data } = await apiClient.get(`/lintrule/all`);
     return data;
 }
 
 export async function getUserLintingRules(): Promise<LintConfigDto[]> {
-    const { data } = await apiClient.get(`/snippets/lintconfig/user`);
+    const { data } = await apiClient.get(`/lintconfig/user`);
     return data;
 }
