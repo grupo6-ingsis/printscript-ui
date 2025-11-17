00b93613de5ff2101aa5917779446a44898d9d04
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-11-17 04:11:09 -0300

    feat: linting rules

--- src/api/linting.api.ts
+++ src/api/linting.api.ts
@@ -1,5 +1,5 @@
 import apiClient from "./apiClient.ts";
-import {LintConfigDto, LintRuleDto} from "../types/Rule.ts";
+import {LintConfigDto, LintRuleDto, Rule} from "../types/Rule.ts";
 
 export async function getLintingRules(): Promise<LintRuleDto[]> {
     const { data } = await apiClient.get(`/lintrule/all`);
@@ -9,4 +9,15 @@ export async function getLintingRules(): Promise<LintRuleDto[]> {
 export async function getUserLintingRules(): Promise<LintConfigDto[]> {
     const { data } = await apiClient.get(`/lintconfig/user`);
     return data;
+}
+
+export async function activateRule(request : Rule): Promise<LintConfigDto>{
+    const { data } = await apiClient.post(`/lintconfig`, request);
+    return data;
+}
+
+export async function deactivateRule(lintRuleId: string): Promise<void>{
+    await apiClient.delete(`/lintconfig`, {
+        params: { lintRuleId }
+    });
 }
