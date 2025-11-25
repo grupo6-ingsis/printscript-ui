824bc5aee1c1603fcf783d903e96810edc8c76c7
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-11-24 22:27:07 -0300

    feat: click language cypress #19

--- cypress/e2e/addSnippet.cy.ts
+++ cypress/e2e/addSnippet.cy.ts
@@ -30,10 +30,7 @@ describe('Add snippet tests', () => {
       cy.get('#description').type('This is a test description');
       cy.get('[data-testid="add-snippet-code-editor"]').click();
     cy.get('[data-testid="add-snippet-code-editor"]').type(`let snippet: String = "some snippet"; \n print(snippet);`);
-      cy.contains('button', 'Save Snippet')
-          .scrollIntoView()
-          .should('be.enabled')
-          .click();
+      cy.contains('button', 'Save Snippet').click({ force: true });
       cy.wait('@postRequest').its('response.statusCode').should('eq', 200);
   })
 
