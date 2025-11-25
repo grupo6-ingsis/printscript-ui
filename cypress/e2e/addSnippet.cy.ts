a5063f17641f8de1257edc47215ac851719fcfd1
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-11-25 00:41:21 -0300

    feat: second test create snippet from file x9

--- cypress/e2e/addSnippet.cy.ts
+++ cypress/e2e/addSnippet.cy.ts
@@ -50,7 +50,7 @@ describe('Add snippet tests', () => {
           .should('be.visible')
           .click();
       cy.get('[data-testid="upload-file-input"]')
-          .selectFile('cypress/fixtures/example.ts', { force: true });
+          .selectFile('cypress/fixtures/example.ps', { force: true });
       cy.contains('button', 'Save Snippet').click({ force: true });
       cy.wait('@postRequest').its('response.statusCode').should('eq', 200);
   })
