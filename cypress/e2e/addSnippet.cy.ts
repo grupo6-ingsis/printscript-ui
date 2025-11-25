9aa1b49c94a21066fdb443f1b612f076b241dadc
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-11-25 01:43:13 -0300

    feat: second test create snippet from file x15

--- cypress/e2e/addSnippet.cy.ts
+++ cypress/e2e/addSnippet.cy.ts
@@ -55,9 +55,7 @@ describe('Add snippet tests', () => {
       cy.get('#name').should('have.value', 'example');
       cy.get('#description').type('This is a test description');
       // Wait for button to be enabled before clicking
-      cy.contains('button', 'Save Snippet')
-          .then($btn => $btn[0].disabled = false)
-          .click();
+      cy.contains('button', 'Save Snippet').should('be.visible').should('not.be.disabled').click({ force: true });
       cy.wait('@postRequest').its('response.statusCode').should('eq', 200);
   })
 })
